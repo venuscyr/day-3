031613e9566edba1f947fb1208aac939879eb7f3
venuscyr <venus.amako@epitech.digital>
2024-10-02 16:31:31 +0200

    ex_06.js

---
+++ ex_06.js
@@ -0,0 +1,15 @@
+function findAddresses(toFind) {
+ 
+ addresses.forEach(address => { 
+   const affich = address[0] + ','+address[1];
+   if (affich.includes{toFind}) {
+   console.log(affich);}
+ });
+ return 
+ }
+ 
+
+// console.log the found addresses for them to be displayed !
+// Write your function here... 
+
+addresses = [["45 avenue des tulipes", "92130 Issy-les-Moulineaux"], ["95 avenue parmentier", "75011 Paris"], ["24 Rue Pasteur", "92400 Courbevoie"], ["Champ de Mars, 5 Av. Anatole France", "75007 Paris"], ["Pl. Louis-Armand", "75571 Paris"]];
