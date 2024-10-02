041a7bee13179d12fdb98384034ae9368c54af24
venuscyr <venus.amako@epitech.digital>
2024-10-02 16:46:28 +0200

    ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,17 +1,13 @@
-function addHousing(hounsingList){
+function addHousing(hounsingList,newHouse,quantity){
 
 let housingList = new Map ();
 addHousing ( housingList , " Flat " , 2)
 addHousing ( housingList , " Log " , 3)
 addHousing ( housingList , " Flat " , 3)
 addHousing ( housingList , " Hutch " , 2)
-displayResult ( housingList ) ;
-
 
 }
 
-
-// Here is some code to test and display your function output:
 // (you must modify it to test out your code but /!\ do not deliver it /!\ ). Only deliver your function
 const result = new Map([["Flat",5],["Log",3],["Hutch",2]]);
 displayResult(result);
