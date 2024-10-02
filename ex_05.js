be5a74f973565cc18062ed713644ab3f380c90ed
venuscyr <venus.amako@epitech.digital>
2024-10-02 14:52:38 +0200

    ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,11 @@
+function getHousesPrototype() {
+const ground = new Map([
+['Apartment 75008', 90],
+['Apartment 94300', 105],
+['Apartment 92300', 140],
+['Apartment 75015', 65]
+]);
+return ground;
+}
+let test = getHousesPrototype();
+console.log(test);
