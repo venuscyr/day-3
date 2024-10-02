cc47a78cd0858ef2f0d33bc132bac2de9e78a133
venuscyr <venus.amako@epitech.digital>
2024-10-02 10:23:18 +0200

    ex_01.js

---
+++ ex_01.js
@@ -0,0 +1,16 @@
+function getAngryDog(numberOfWoofs)
+{
+ for(let i = 0; i < 5; i = i +1   ) {
+ return ("woofwoofwoofwoofwoof")
+ 
+ console.log("woofwoofwoofwoofwoof")
+ 
+  
+ }
+}
+
+
+
+//Some code to test your function output : (you can modify it but /!\ do not deliver it /!\ ). Only deliver your function
+var result = getAngryDog(3);
+displayResult(result);
